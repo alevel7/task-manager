8707c19443a6fa016204a37d047a16202bc4edff
kazeem taiwo <alevel7@gmail.com>
2022-03-20 21:59:47 +0100

    add task repo

---
+++ src/tasks/task.repository.ts
@@ -0,0 +1,16 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { EntityRepository, Repository,DataSource } from 'typeorm'
+import { Task } from './task.entity';
+
+@EntityRepository(Task)
+export class TasksRepository extends Repository<Task> {
+
+ 
+}
+// const dataSource = new DataSource()
+
+// export const TaskRepository = dataSource.getRepository(Task)
+
+// export class TaskController {
+
+// }
